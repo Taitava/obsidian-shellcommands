7f7b1711fa2d731f4e7b46ec96b08bfbcc760364
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-09-15 18:29:51 +0300

    Restructure: Fix variable instructions order.

--- src/variables/ShellCommandVariableInstructions.ts
+++ src/variables/ShellCommandVariableInstructions.ts
@@ -6,6 +6,7 @@ interface ShellCommandVariableInstruction {
 let shell_command_variable_instructions: ShellCommandVariableInstruction[] = [];
 
 export function getShellCommandVariableInstructions() {
+    sort_shell_command_variable_instructions(); // Make sure the variables are in correct order.
     return shell_command_variable_instructions;
 }
 
@@ -13,5 +14,19 @@ export function addShellCommandVariableInstructions(variable_name: string, instr
     shell_command_variable_instructions.push({
         variable_name: variable_name,
         instructions: instructions,
-    })
+    });
+}
+
+/**
+ * Sorts shell_command_variable_instructions alphabetically based on variable_name.
+ */
+function sort_shell_command_variable_instructions() {
+    shell_command_variable_instructions.sort((a: ShellCommandVariableInstruction, b: ShellCommandVariableInstruction) => {
+        if (a.variable_name < b.variable_name) {
+            return -1;
+        } else if (a.variable_name > b.variable_name) {
+            return 1;
+        }
+        return 0;
+    });
 }
