914233031d53fa25e9b3316c7b2581319c7b1f30
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-11-02 15:44:23 +0200

    #94 and #11: AllSpecialCharactersEscaper: Fix One occurrence of #94 bug.

--- src/variables/escapers/AllSpecialCharactersEscaper.ts
+++ src/variables/escapers/AllSpecialCharactersEscaper.ts
@@ -7,6 +7,10 @@ export abstract class AllSpecialCharactersEscaper extends Escaper {
     protected abstract prefix: string;
 
     public escape(): string {
-        return this.raw_value.replace(/[^\w\d]/g, this.prefix + "$&"); // /g means to replace all occurrences instead of just the first one. $& means "the whole match".
+        return this.raw_value.replace(/[^\w\d]/g, (special_character: string) => {  // /g means to replace all occurrences instead of just the first one.
+            // Do the replacing in a function in order to avoid a possible $ character to be interpreted by JavaScript to interact with the regex.
+            // More information: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_a_parameter (referenced 2021-11-02.
+            return this.prefix + special_character;
+        });
     }
 }
