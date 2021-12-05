133986265f4b2d1bb2e5fb854483c82041c520be
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-12-05 18:15:57 +0200

    #33: Autocompletion documentation link is tested.

--- src/Documentation.ts
+++ src/Documentation.ts
@@ -1,5 +1,5 @@
 export const DocumentationMainLink = "https://publish.obsidian.md/shellcommands";
 export const DocumentationVariablesLink = "https://publish.obsidian.md/shellcommands/Variables/Variables+-+general+principles#All+variables";
-export const DocumentationAutocompleteLink = "https://publish.obsidian.md/shellcommands/Variables/Autocomplete/Autocomplete"; // TODO: Check that the link is correct, it's written here before creating the page.
+export const DocumentationAutocompleteLink = "https://publish.obsidian.md/shellcommands/Variables/Autocomplete/Autocomplete";
 export const GitHubLink = "https://github.com/Taitava/obsidian-shellcommands";
 export const ChangelogLink = "https://github.com/Taitava/obsidian-shellcommands/blob/main/CHANGELOG.md";
