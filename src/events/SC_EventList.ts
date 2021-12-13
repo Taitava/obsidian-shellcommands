bef7955e5fc031af8c34f91bc8a4e3adda555971
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-12-13 11:44:35 +0200

    #123: Events: Create EveryNSeconds.

--- src/events/SC_EventList.ts
+++ src/events/SC_EventList.ts
@@ -2,6 +2,7 @@ import ShellCommandsPlugin from "../main";
 import {SC_Event_onLayoutReady} from "./SC_Event_onLayoutReady";
 import {SC_Event_onQuit} from "./SC_Event_onQuit";
 import {SC_Event_onActiveLeafChanged} from "./SC_Event_onActiveLeafChanged";
+import {SC_Event_EveryNSeconds} from "./SC_Event_EveryNSeconds";
 
 export function getSC_Events(plugin: ShellCommandsPlugin) {
     if (undefined === getSC_Events.events) {
@@ -10,6 +11,7 @@ export function getSC_Events(plugin: ShellCommandsPlugin) {
             new SC_Event_onLayoutReady(plugin),
             new SC_Event_onQuit(plugin),
             new SC_Event_onActiveLeafChanged(plugin),
+            new SC_Event_EveryNSeconds(plugin),
         ];
     }
     return getSC_Events.events;
