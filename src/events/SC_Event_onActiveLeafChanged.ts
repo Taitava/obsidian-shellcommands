37755cee4ca932ec53dbfd172846d87280640fab
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2021-12-19 12:43:16 +0200

    #123: Events: Change 'After active leaf has changed' to 'After switching the active pane'.

--- src/events/SC_Event_onActiveLeafChanged.ts
+++ src/events/SC_Event_onActiveLeafChanged.ts
@@ -2,6 +2,6 @@ import {SC_WorkspaceEvent} from "./SC_WorkspaceEvent";
 
 export class SC_Event_onActiveLeafChanged extends SC_WorkspaceEvent {
     protected readonly event_code = "on-active-leaf-changed";
-    protected readonly event_title = "After active leaf has changed";
+    protected readonly event_title = "After switching the active pane";
     protected readonly workspace_event = "active-leaf-change";
 }
