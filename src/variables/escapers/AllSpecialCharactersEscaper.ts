ffcedc069091effd5114ba7a43d92b31d089354b
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-03-03 20:28:18 +0200

    Fix #171: Variable escaping corrupted four-byte unicode characters, e.g. emojis.

--- src/variables/escapers/AllSpecialCharactersEscaper.ts
+++ src/variables/escapers/AllSpecialCharactersEscaper.ts
@@ -7,7 +7,7 @@ export abstract class AllSpecialCharactersEscaper extends Escaper {
     protected abstract prefix: string;
 
     public escape(): string {
-        return this.raw_value.replace(/[^\w\d]/g, (special_character: string) => {  // /g means to replace all occurrences instead of just the first one.
+        return this.raw_value.replace(/[^\w\d]/gu, (special_character: string) => {  // /g means to replace all occurrences instead of just the first one. /u means to handle four-byte unicode characters correctly as one character, not as two separate characters.
             // Do the replacing in a function in order to avoid a possible $ character to be interpreted by JavaScript to interact with the regex.
             // More information: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace#specifying_a_string_as_a_parameter (referenced 2021-11-02.
             return this.prefix + special_character;
