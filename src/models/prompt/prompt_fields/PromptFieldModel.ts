Taitava/obsidian-shellcommands

commit fc1b15e286b6ca3f78f324b69ecc3dd2e0f40e44

#161: Model.createInstances(): Allow returning a Set instead of an array.
Also convert CustomVariableModel and PromptFieldModel to use Sets.

src/models/prompt/prompt_fields/PromptFieldModel.ts
@@ -23,16 +23,14 @@ export class PromptFieldModel extends Model {
         };
     }
 
-    public createInstances(prompt: Prompt): Map<number, PromptField> {
-        const prompt_fields = new PromptFieldMap;
+    public createInstances(prompt: Prompt): PromptFieldSet {
+        const prompt_fields = new PromptFieldSet;
         let index = 0;
         prompt.configuration.fields.forEach((field_configuration: PromptFieldConfiguration) => {
-            prompt_fields.set( // TODO: Change to a Set instead of a Map. Then the index variable can be removed from this method.
-                index, // 0-indexed
+            prompt_fields.add(
                 // TODO: When the 'type' field gets implemented on PromptFieldConfiguration, implement some kind of switch structure here to create different types of PromptFields.
                 new PromptField_Text(this, prompt, field_configuration, index), // TODO: Extract this to a separate method.
             );
-            index++;
         });
         return prompt_fields;
     }
@@ -158,11 +156,11 @@ export class PromptFieldModel extends Model {
     }
 
     protected _deleteInstance(prompt_field: PromptField): void {
-        prompt_field.prompt.prompt_fields.delete(prompt_field.prompt_field_index as number); // TODO: Remove 'as number' after prompt_fields in changed to be a Set instead of a Map.
+        prompt_field.prompt.prompt_fields.delete(prompt_field);
     }
 }
 
-export class PromptFieldMap extends Map<number, PromptField> {} // TODO: Consider making this to be a Set instead of a Map, as id's are not used here.
+export class PromptFieldSet extends Set<PromptField> {}
 
 export interface PromptFieldSettingGroup {
     heading_setting: Setting;

src/main.ts
@@ -32,7 +32,7 @@ import {addCustomAutocompleteItems} from "./settings/setting_elements/Autocomple
 import {getSC_Events} from "./events/SC_EventList";
 import {SC_Event} from "./events/SC_Event";
 import {
-	CustomVariableInstance,
+	CustomVariableInstanceSet,
 	CustomVariableModel,
 	getModel,
 	introduceModels,
@@ -52,7 +52,7 @@ export default class SC_Plugin extends Plugin {
 	public obsidian_commands: ObsidianCommandsContainer = {};
 	private t_shell_commands: TShellCommandContainer = {};
 	private prompts: PromptMap;
-	private custom_variable_instances: CustomVariableInstance[];
+	private custom_variable_instances: CustomVariableInstanceSet;
 
 	/**
 	 * Holder for shell commands and aliases, whose variables are parsed before the actual execution during command
@@ -133,7 +133,7 @@ export default class SC_Plugin extends Plugin {
 		return this.prompts;
 	}
 
-	public getCustomVariableInstances(): CustomVariableInstance[] {
+	public getCustomVariableInstances(): CustomVariableInstanceSet {
 		return this.custom_variable_instances;
 	}
 

src/models/Model.ts
@@ -20,7 +20,7 @@ export abstract class Model {
      * Creates instance objects from already existing configuration. I.e. does not create NEW instances or new configurations.
      * TODO: Rename this method to loadInstances().
      */
-    public abstract createInstances(parent_instance_or_configuration: Instance | InstanceConfiguration): object[] | Map<string | number, Instance>; // TODO: Change so that only Map is allowed. TODO: Consider changing so that Set is also allowed, in which case remove 'number' possibility from Map.
+    public abstract createInstances(parent_instance_or_configuration: Instance | InstanceConfiguration): Set<Instance> | Map<string, Instance>;
 
     /**
      * Creates a new instance and adds its configuration to the parent configuration.

src/models/custom_variable/CustomVariableModel.ts
@@ -9,7 +9,7 @@ import {
 
 export class CustomVariableModel extends Model {
 
-    private custom_variable_instances: CustomVariableInstance[];
+    private custom_variable_instances: CustomVariableInstanceSet;
 
     public getSingularName(): string {
         return "Custom variable";
@@ -25,10 +25,12 @@ export class CustomVariableModel extends Model {
         };
     }
 
-    public createInstances(parent_configuration: SC_MainSettings): CustomVariableInstance[] {
-        this.custom_variable_instances = [];
+    public createInstances(parent_configuration: SC_MainSettings): CustomVariableInstanceSet {
+        this.custom_variable_instances = new CustomVariableInstanceSet;
         parent_configuration.custom_variables.forEach((custom_variable_configuration: CustomVariableConfiguration, custom_variable_index: number) => {
-            this.custom_variable_instances[custom_variable_index] = new CustomVariableInstance( this, custom_variable_configuration, parent_configuration, custom_variable_index);
+            this.custom_variable_instances.add(
+                new CustomVariableInstance(this, custom_variable_configuration, parent_configuration, custom_variable_index)
+            );
         });
         return this.custom_variable_instances;
     }
@@ -37,7 +39,7 @@ export class CustomVariableModel extends Model {
         const custom_variable_configuration: CustomVariableConfiguration = this._getDefaultConfiguration();
         const custom_variable_instance = new CustomVariableInstance(this, custom_variable_configuration, parent_configuration, parent_configuration.custom_variables.length);
         parent_configuration.custom_variables.push(custom_variable_configuration);
-        this.custom_variable_instances.push(custom_variable_instance);
+        this.custom_variable_instances.add(custom_variable_instance);
         return custom_variable_instance;
         // TODO: Move this logic to the base Model class.
     }
@@ -66,7 +68,7 @@ export class CustomVariableModel extends Model {
     }
 
     protected _deleteInstance(custom_variable_instance: CustomVariableInstance): void {
-        this.custom_variable_instances.remove(custom_variable_instance);
+        this.custom_variable_instances.delete(custom_variable_instance);
     }
 }
 
@@ -74,3 +76,5 @@ export interface CustomVariableConfiguration {
     id: string,
     name: string,
 }
+
+export class CustomVariableInstanceSet extends Set<CustomVariableInstance> {}

src/models/prompt/Prompt.ts
@@ -6,7 +6,7 @@ import {
     Instance,
     PromptField,
     PromptFieldConfiguration,
-    PromptFieldMap,
+    PromptFieldSet,
     PromptFieldModel,
     PromptModal,
     PromptModel,
@@ -14,7 +14,7 @@ import {
 
 export class Prompt extends Instance {
 
-    public prompt_fields: PromptFieldMap = new PromptFieldMap();
+    public prompt_fields: PromptFieldSet = new PromptFieldSet();
 
     constructor(
         public model: PromptModel,

src/models/prompt/PromptModal.ts
@@ -5,7 +5,7 @@ import {Setting} from "obsidian";
 import {
     Prompt,
     PromptField,
-    PromptFieldMap,
+    PromptFieldSet,
 } from "../../imports";
 
 export class PromptModal extends SC_Modal {
@@ -17,7 +17,7 @@ export class PromptModal extends SC_Modal {
 
     constructor(
         plugin: SC_Plugin,
-        private readonly prompt_fields: PromptFieldMap,
+        private readonly prompt_fields: PromptFieldSet,
         private readonly t_shell_command: TShellCommand,
         private readonly prompt: Prompt,
 

commit c5934e7139f9f9990a9ce7b73e3aa1fda4cf9501

#37: PromptFieldModel: Fix deletion always removed the first field.
Was broken in commit: fc1b15e286b6ca3f78f324b69ecc3dd2e0f40e44

src/models/prompt/prompt_fields/PromptFieldModel.ts
@@ -26,8 +26,7 @@ export class PromptFieldModel extends Model {
 
     public loadInstances(prompt: Prompt): PromptFieldSet {
         const prompt_fields = new PromptFieldSet;
-        let index = 0;
-        prompt.configuration.fields.forEach((field_configuration: PromptFieldConfiguration) => {
+        prompt.configuration.fields.forEach((field_configuration: PromptFieldConfiguration, index) => {
             prompt_fields.add(
                 this.createInstance(prompt, field_configuration, index)
             );