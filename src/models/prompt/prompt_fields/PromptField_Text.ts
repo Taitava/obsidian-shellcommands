061fc9e1e9a2ced49f322f93717e2508bd11f3c5
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-03-12 19:30:39 +0200

    #37: PromptField_Text: Use autocomplete menu for prompt fields.

--- src/models/prompt/prompt_fields/PromptField_Text.ts
+++ src/models/prompt/prompt_fields/PromptField_Text.ts
@@ -2,6 +2,9 @@ import {
     Setting,
     TextComponent,
 } from "obsidian";
+import {createAutocomplete} from "../../../settings/setting_elements/Autocomplete";
+import {getVariableAutocompleteItems} from "../../../variables/getVariableAutocompleteItems";
+import SC_Plugin from "../../../main";
 import {SC_Event} from "../../../events/SC_Event";
 import {
     PromptField,
@@ -12,13 +15,23 @@ export class PromptField_Text extends PromptField {
     private text_component: TextComponent;
 
     protected _createField(container_element: HTMLElement, sc_event: SC_Event | null) {
-        new Setting(container_element)
+        const plugin: SC_Plugin = this.prompt.model.plugin;
+
+        // Create the field
+        const on_change = () => this.valueHasChanged(sc_event);
+        const setting = new Setting(container_element)
             .setName(this.configuration.label)
             .addText((text_component) => {
                 this.text_component = text_component;
-                text_component.onChange(() => this.valueHasChanged(sc_event));
+                text_component.onChange(on_change);
             })
         ;
+
+        // Show autocomplete menu (if enabled)
+        if (plugin.settings.show_autocomplete_menu) {
+            const input_element = setting.controlEl.find("input") as HTMLInputElement;
+            createAutocomplete(input_element, getVariableAutocompleteItems(plugin), on_change);
+        }
     }
 
     protected setValue(value: string): void {
