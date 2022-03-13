b0bca67fc18f2bb42bee949875b98ad6cc7ed529
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-03-13 08:15:59 +0200

    WIP: 1/3 #185: SC_Plugin: Extract execution related methods to a new class and file.
    No content changes are done in this commit in order to allow Git to better follow the change history.
    
    main.ts is renamed temporarily in order to make Git notice that the content of the new file ShellCommandExecutor.ts is not actually new content, but originates from an old file.
    
    main-RENAMED-TEMPORARILY.ts will be renamed back to main.ts in the next commit.

--- src/main-RENAMED-TEMPORARILY.ts
+++ src/main-RENAMED-TEMPORARILY.ts
@@ -1,11 +1,9 @@
 import {Command, Notice, Plugin} from 'obsidian';
-import {exec, ExecException, ExecOptions} from "child_process";
 import {
 	combineObjects,
 	generateObsidianCommandName,
 	getOperatingSystem,
 	getPluginAbsolutePath,
-	getVaultAbsolutePath,
 } from "./Common";
 import {RunMigrations} from "./Migrations";
 import {
@@ -21,11 +19,8 @@ import {ObsidianCommandsContainer} from "./ObsidianCommandsContainer";
 import {SC_MainSettingsTab} from "./settings/SC_MainSettingsTab";
 import * as path from "path";
 import * as fs from "fs";
-import {ConfirmExecutionModal} from "./ConfirmExecutionModal";
-import {handleShellCommandOutput} from "./output_channels/OutputChannelDriverFunctions";
-import {BaseEncodingOptions} from "fs";
 import {ParsingResult, TShellCommand, TShellCommandContainer} from "./TShellCommand";
-import {getUsersDefaultShell, isShellSupported} from "./Shell";
+import {getUsersDefaultShell} from "./Shell";
 import {versionCompare} from "./lib/version_compare";
 import {debugLog, setDEBUG_ON} from "./Debug";
 import {addCustomAutocompleteItems} from "./settings/setting_elements/Autocomplete";
@@ -40,7 +35,6 @@ import {
 	CustomVariableModel,
 	getModel,
 	introduceModels,
-	Preaction,
 	PromptMap,
 	PromptModel,
 } from "./imports";
@@ -298,169 +292,6 @@ export default class SC_Plugin extends Plugin {
 
 	public generateObsidianCommandId(shell_command_id: string) {
 		return "shell-command-" + shell_command_id;
-	}
-
-	/**
-	 * Performs preactions, and if they all give resolved Promises, executes the shell command.
-	 *
-	 * TODO: Change the name of this method to reflect the new behavior.
-	 *
-	 * @param t_shell_command Used for reading other properties. t_shell_command.shell_command won't be used!
-	 * @param shell_command_parsing_result The actual shell command that will be executed.
-	 * @param sc_event Needed for Preactions to be able to access all variables, in case any variables are used by a Preaction. Use null, if the shell command execution happens outside of any event context.
-	 */
-	public confirmAndExecuteShellCommand(t_shell_command: TShellCommand, shell_command_parsing_result: ParsingResult, sc_event: SC_Event | null) {
-
-		// Perform preactions before execution
-		const preactions = t_shell_command.getPreactions(shell_command_parsing_result, sc_event);
-		let preaction_pipeline = Promise.resolve(); // Will contain a series of preaction performs.
-		preactions.forEach((preaction: Preaction) => {
-			preaction_pipeline = preaction_pipeline.then(() => {
-				return preaction.perform();
-			});
-		});
-		preaction_pipeline.then(() => {
-			// Execute
-
-			// TODO: This confirmation check should be migrated into a Preaction. We should go now directly to the execution part.
-			// Check if the command needs confirmation before execution
-			if (t_shell_command.getConfirmExecution()) {
-				// Yes, a confirmation is needed.
-				// Open a confirmation modal.
-				new ConfirmExecutionModal(this, shell_command_parsing_result, t_shell_command)
-					.open()
-				;
-				return; // Do not execute now. The modal will call executeShellCommand() later if needed.
-				// TODO: This confirmation check should be migrated into a Preaction.
-			} else {
-				// No need to confirm.
-				// Execute.
-				this.executeShellCommand(t_shell_command, shell_command_parsing_result);
-			}
-		}).catch(() => {
-			// Cancel execution
-			debugLog("Shell command execution cancelled.")
-		});
-
-
-	}
-
-	/**
-	 * Does not ask for confirmation before execution. This should only be called if: a) a confirmation is already asked from a user, or b) this command is defined not to need a confirmation.
-	 * Use confirmAndExecuteShellCommand() instead to have a confirmation asked before the execution.
-	 *
-	 * @param t_shell_command Used for reading other properties. t_shell_command.shell_command won't be used!
-	 * @param shell_command_parsing_result The actual shell command that will be executed is taken from this object's '.shell_command' property.
-	 */
-	public executeShellCommand(t_shell_command: TShellCommand, shell_command_parsing_result: ParsingResult) {
-		const working_directory = this.getWorkingDirectory();
-
-		// Check that the shell command is not empty
-		const shell_command = shell_command_parsing_result.shell_command.trim();
-		if (!shell_command.length) {
-			// It is empty
-			debugLog("The shell command is empty. :(");
-			this.newError("The shell command is empty :(");
-			return;
-		}
-
-		// Check that the currently defined shell is supported by this plugin. If using system default shell, it's possible
-		// that the shell is something that is not supported. Also, the settings file can be edited manually, and incorrect
-		// shell can be written there.
-		const shell = t_shell_command.getShell();
-		if (!isShellSupported(shell)) {
-			debugLog("Shell is not supported: " + shell);
-			this.newError("This plugin does not support the following shell: " + shell);
-			return;
-		}
-
-
-		// Check that the working directory exists and is a folder
-		if (!fs.existsSync(working_directory)) {
-			// Working directory does not exist
-			// Prevent execution
-			debugLog("Working directory does not exist: " + working_directory);
-			this.newError("Working directory does not exist: " + working_directory);
-		}
-		else if (!fs.lstatSync(working_directory).isDirectory()) {
-			// Working directory is not a directory.
-			// Prevent execution
-			debugLog("Working directory exists but is not a folder: " + working_directory);
-			this.newError("Working directory exists but is not a folder: " + working_directory);
-		} else {
-			// Working directory is OK
-			// Prepare execution options
-			const options: BaseEncodingOptions & ExecOptions = {
-				"cwd": working_directory,
-				"shell": shell,
-			};
-
-			// Execute the shell command
-			debugLog("Executing command " + shell_command + " in " + working_directory + "...");
-			exec(shell_command, options, (error: ExecException|null, stdout: string, stderr: string) => {
-
-				// Did the shell command execute successfully?
-				if (null !== error) {
-					// Some error occurred
-					debugLog("Command executed and failed. Error number: " + error.code + ". Message: " + error.message);
-
-					// Check if this error should be displayed to the user or not
-					if (t_shell_command.getIgnoreErrorCodes().contains(error.code)) {
-						// The user has ignored this error.
-						debugLog("User has ignored this error, so won't display it.");
-
-						// Handle only stdout output stream
-						handleShellCommandOutput(this, t_shell_command, shell_command_parsing_result, stdout, "", null);
-					} else {
-						// Show the error.
-						debugLog("Will display the error to user.");
-
-						// Check that stderr actually contains an error message
-						if (!stderr.length) {
-							// Stderr is empty, so the error message is probably given by Node.js's child_process.
-							// Direct error.message to the stderr variable, so that the user can see error.message when stderr is unavailable.
-							stderr = error.message;
-						}
-
-						// Handle both stdout and stderr output streams
-						handleShellCommandOutput(this, t_shell_command, shell_command_parsing_result, stdout, stderr, error.code);
-					}
-				} else {
-					// Probably no errors, but do one more check.
-
-					// Even when 'error' is null and everything should be ok, there may still be error messages outputted in stderr.
-					if (stderr.length > 0) {
-						// Check a special case: should error code 0 be ignored?
-						if (t_shell_command.getIgnoreErrorCodes().contains(0)) {
-							// Exit code 0 is on the ignore list, so suppress stderr output.
-							stderr = "";
-							debugLog("Shell command executed: Encountered error code 0, but stderr is ignored.");
-						} else {
-							debugLog("Shell command executed: Encountered error code 0, and stderr will be relayed to an output handler.");
-						}
-					} else {
-						debugLog("Shell command executed: No errors.");
-					}
-
-					// Handle output
-					handleShellCommandOutput(this, t_shell_command, shell_command_parsing_result, stdout, stderr, 0); // Use zero as an error code instead of null (0 means no error). If stderr happens to contain something, exit code 0 gets displayed in an error balloon (if that is selected as a driver for stderr).
-				}
-			});
-		}
-	}
-
-	private getWorkingDirectory() {
-		// Returns either a user defined working directory, or an automatically detected one.
-		const working_directory = this.settings.working_directory;
-		if (working_directory.length == 0) {
-			// No working directory specified, so use the vault directory.
-			return getVaultAbsolutePath(this.app);
-		} else if (!path.isAbsolute(working_directory)) {
-			// The working directory is relative.
-			// Help to make it refer to the vault's directory. Without this, the relative path would refer to Obsidian's installation directory (at least on Windows).
-			return path.join(getVaultAbsolutePath(this.app), working_directory);
-		}
-		return working_directory;
 	}
 
 	public onunload() {
