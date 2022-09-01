388350c5b2e5c49a4d86c375124d7bd441e7efa7
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-09-01 14:33:18 +0300

    WIP: #77: {{note_content}} variable: Update the class to meet past changes in the variable system.
    Re: 8b4a993933b2022f3402c7d77a140f098e34ca60

---
+++ src/variables/Variable_NoteContent.ts
@@ -0,0 +1,21 @@
+import {FileVariable} from "./FileVariable";
+
+export class Variable_NoteContent extends FileVariable {
+    public variable_name = "note_content";
+    public help_text = "Gets the current note's content without YAML frontmatter.";
+
+    protected generateValue(): string {
+        const active_file = this.getFile();
+        if (!active_file) {
+            return null; // null indicates that getting a value has failed and the command should not be executed.
+        }
+
+        const content = this.app.vault.read(active_file);
+        content.then((file_content) => {
+            // TODO: process file content: 1) remove YAML frontmatter, 2) return the result
+        });
+
+        this.newErrorMessage("This variable does not work yet.");
+        return null;
+    }
+}
