e99998a787d48de0ff3e9845eacd8754a141b849
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-11-03 20:05:53 +0200

    #282: Create {{caret_paragraph}} variable.

---
+++ src/variables/Variable_CaretParagraph.ts
@@ -0,0 +1,34 @@
+/*
+ * 'Shell commands' plugin for Obsidian.
+ * Copyright (C) 2021 - 2022 Jarkko Linnanvirta
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.0 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Contact the author (Jarkko Linnanvirta): https://github.com/Taitava/
+ */
+
+import {EditorVariable} from "./EditorVariable";
+
+export class Variable_CaretParagraph extends EditorVariable {
+    public variable_name = "caret_paragraph";
+    public help_text = "Gives a text line at the current caret position.";
+
+    protected async generateValue(): Promise<string | null> {
+        if (!this.requireEditor() || !this.isViewModeSource()) {
+            return null;
+        }
+        const caretPosition = this.editor.getCursor('to');
+        return this.editor.getLine(caretPosition.line);
+    }
+
+}
