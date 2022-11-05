de4ca13d99a11bc8c05c044d3eb4ab47e8ee61e1
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2022-11-05 09:46:05 +0200

    #281: isShellSupported(): Make shell matching more strict.
    Note that this will make the function not to recognise `sh` as a supported shell. I'll re-add that support in the next commit.

--- src/Shell.ts
+++ src/Shell.ts
@@ -49,8 +49,9 @@ export function isShellSupported(shell: string) {
     const shell_file_name = extractFileName(shell);
     const supported_shells = PlatformShells[getOperatingSystem()];
     for (const supported_shell_path in supported_shells) {
-        if (supported_shell_path.substr(-shell_file_name.length, shell_file_name.length).toLowerCase() === shell_file_name.toLowerCase()) {
-            // If supported_shell_path (e.g. /bin/bash or CMD.EXE) ends with shell_file_name (e.g. bash, derived from /bin/bash or CMD.EXE, derived from C:\System32\CMD.EXE), then the shell can be considered to be supported.
+        // Check that the shell file names match. It doesn't matter in which directory the shell is located in.
+        if (extractFileName(supported_shell_path).toLowerCase() === shell_file_name.toLowerCase()) {
+            // The shell can be considered to be supported.
             return true;
         }
     }
