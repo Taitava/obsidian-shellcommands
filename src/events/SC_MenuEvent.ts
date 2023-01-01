a586ab480b0048ce889e5b57f1af775821085981
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2023-01-01 13:51:36 +0200

    SC_MenuEvent.addTShellCommandToMenu(): Fix accessing a menu item possibly too early.
    Not directly related to #70, but this was noticed during it, and as I haven't noticed this glitch causing problems in practise, there's no need to create a separate issue.
    
    The original plan was to create a separate method, but then parsing_results would have become inaccessible.

--- src/events/SC_MenuEvent.ts
+++ src/events/SC_MenuEvent.ts
@@ -19,46 +19,41 @@
 
 import {SC_WorkspaceEvent} from "./SC_WorkspaceEvent";
 import {ShellCommandParsingProcess, TShellCommand} from "../TShellCommand";
-import {
-    Menu,
-    MenuItem,
-} from "obsidian";
+import {Menu} from "obsidian";
 import {ParsingResult} from "../variables/parseVariables";
 
 export abstract class SC_MenuEvent extends SC_WorkspaceEvent {
 
     protected async addTShellCommandToMenu(t_shell_command: TShellCommand, menu: Menu) {
         // Create the menu item as soon as possible. (If it's created after 'await parsing_process.process()' below, it won't be shown in the menu for some reason, at least in Obsidian 0.16.1).
-        // No title is set here, it will be set later.
-        let menu_item: MenuItem;
-        menu.addItem(item => menu_item = item
-            .setIcon(t_shell_command.getIconId()) // Icon id can be null.
-            .onClick(async () => {
-                await this.trigger(
-                    t_shell_command,
-                    parsing_process,
-                );
-            }),
-        );
-
-        // Parse shell command variables to get a title
-        let title = t_shell_command.getAliasOrShellCommand(); // May contain unparsed variables.
-        let parsing_process: ShellCommandParsingProcess;
-        if (this.plugin.settings.preview_variables_in_command_palette) {
-            // Start a parsing process
-            parsing_process = t_shell_command.createParsingProcess(this);
-            if (await parsing_process.process()) {
-                // Parsing succeeded.
-                const parsing_results = parsing_process.getParsingResults();
-                const aliasParsingResult: ParsingResult = parsing_results["alias"] as ParsingResult; // as ParsingResult: Tells TypeScript that the object exists.
-                const shellCommandParsingResult: ParsingResult = parsing_results["shell_command"] as ParsingResult; // as ParsingResult: Tells TypeScript that the object exists.
-                title = aliasParsingResult.parsed_content || shellCommandParsingResult.parsed_content as string; // Try to use a parsed alias, but if no alias is available, use a parsed shell command instead. as string = parsed shell command always exist when the parsing itself has succeeded.
+        menu.addItem(async menuItem => {
+            // Parse shell command variables to get a title
+            let title = t_shell_command.getAliasOrShellCommand(); // May contain unparsed variables.
+            let parsing_process: ShellCommandParsingProcess;
+            if (this.plugin.settings.preview_variables_in_command_palette) {
+                // Start a parsing process
+                parsing_process = t_shell_command.createParsingProcess(this);
+                if (await parsing_process.process()) {
+                    // Parsing succeeded.
+                    const parsing_results = parsing_process.getParsingResults();
+                    const aliasParsingResult: ParsingResult = parsing_results["alias"] as ParsingResult; // as ParsingResult: Tells TypeScript that the object exists.
+                    const shellCommandParsingResult: ParsingResult = parsing_results["shell_command"] as ParsingResult; // as ParsingResult: Tells TypeScript that the object exists.
+                    title = aliasParsingResult.parsed_content || shellCommandParsingResult.parsed_content as string; // Try to use a parsed alias, but if no alias is available, use a parsed shell command instead. as string = parsed shell command always exist when the parsing itself has succeeded.
+                }
+                // If parsing process fails, the failed process can be passed to this.trigger(). The execution will eventually be cancelled and error messages displayed (if displaying is allowed).
+                menuItem.setTitle(title);
             }
-            // If parsing process fails, the failed process can be passed to this.trigger(). The execution will eventually be cancelled and error messages displayed (if displaying is allowed).
-        }
 
-        // Update menu item title.
-        // @ts-ignore. Suppress for now. // FIXME: Fix in a separate commit by moving the whole title generation block to a new method and call that method from the menu.addItem()'s callback function.
-        menu_item.setTitle(title);
+            // Icon and onClick handler.
+            menuItem
+                .setIcon(t_shell_command.getIconId()) // Icon id can be null.
+                .onClick(async () => {
+                    await this.trigger(
+                        t_shell_command,
+                        parsing_process,
+                    );
+                })
+            ;
+        });
     }
 }
