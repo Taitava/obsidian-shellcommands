Taitava/obsidian-shellcommands

commit b15a13837a095930593b1587b704a1d44bd4a862

#330: Convert shell commands' 'output_channels' from strings to configurable objects.
Also, start renaming "output channel" terms to "output handlers". They are not renamed everywhere yet, though.

src/output_channels/OutputChannelFunctions.ts
@@ -23,11 +23,11 @@ import {OutputChannel, OutputChannels} from "./OutputChannel";
 import {OutputChannel_CurrentFileCaret} from "./OutputChannel_CurrentFileCaret";
 import {OutputChannel_CurrentFileTop} from "./OutputChannel_CurrentFileTop";
 import {
-    OutputChannelCode,
-    OutputChannelCodes,
+    OutputHandlerCode,
+    OutputHandlerConfigurations,
     OutputHandlingMode,
     OutputStream,
-} from "./OutputChannelCode";
+} from "./OutputHandlerCode";
 import {OutputChannel_StatusBar} from "./OutputChannel_StatusBar";
 import {OutputChannel_CurrentFileBottom} from "./OutputChannel_CurrentFileBottom";
 import {OutputChannel_Clipboard} from "./OutputChannel_Clipboard";
@@ -63,7 +63,7 @@ registerOutputChannel("modal", OutputChannel_Modal);
  * @param stdout
  * @param stderr
  * @param error_code TODO: Rename to exitCode everywhere in the codebase.
- * @param output_channels
+ * @param outputHandlers
  */
 export function handleBufferedOutput(
         plugin: SC_Plugin,
@@ -72,7 +72,7 @@ export function handleBufferedOutput(
         stdout: string,
         stderr: string,
         error_code: number | null,
-        output_channels: OutputChannelCodes
+        outputHandlers: OutputHandlerConfigurations,
     ): void {
     // Terminology: Stream = outputs stream from a command, can be "stdout" or "stderr". Channel = a method for this application to present the output ot user, e.g. "notification".
 
@@ -116,14 +116,14 @@ export function handleBufferedOutput(
     }
 
     // Should stderr be processed same time with stdout?
-    if (output_channels.stdout === output_channels.stderr) {
+    if (outputHandlers.stdout === outputHandlers.stderr) {
         // Stdout and stderr use the same channel.
         // Make one handling call.
         handle_stream(
             plugin,
             t_shell_command,
             shell_command_parsing_result,
-            output_channels.stdout,
+            outputHandlers.stdout.handler,
             output,
             error_code,
         );
@@ -132,7 +132,7 @@ export function handleBufferedOutput(
         // Make two handling calls.
         let output_stream_name: OutputStream;
         for (output_stream_name in output) {
-            const output_channel_name = output_channels[output_stream_name];
+            const output_channel_name = outputHandlers[output_stream_name].handler;
             const output_message = output[output_stream_name];
             const separated_output: OutputStreams = {};
             separated_output[output_stream_name] = output_message;
@@ -153,7 +153,7 @@ async function handle_stream(
         plugin: SC_Plugin,
         t_shell_command: TShellCommand,
         shell_command_parsing_result: ShellCommandParsingResult,
-        output_channel_name: OutputChannelCode,
+        output_channel_name: OutputHandlerCode,
         output: OutputStreams,
         error_code: number|null
     ): Promise<void> {
@@ -186,16 +186,16 @@ export function startRealtimeOutputHandling(
         plugin: SC_Plugin,
         tShellCommand: TShellCommand,
         shellCommandParsingResult: ShellCommandParsingResult,
-        outputChannelCodes: OutputChannelCodes,
+        outputHandlerConfigurations: OutputHandlerConfigurations,
         processTerminator: (() => void) | null,
     ): OutputChannels {
 
     const outputChannels: OutputChannels = {};
 
     // stdout
-    if ("ignore" !== outputChannelCodes.stdout) {
+    if ("ignore" !== outputHandlerConfigurations.stdout.handler) {
         outputChannels.stdout = initializeOutputChannel(
-            outputChannelCodes.stdout,
+            outputHandlerConfigurations.stdout.handler,
             plugin,
             tShellCommand,
             shellCommandParsingResult,
@@ -205,14 +205,14 @@ export function startRealtimeOutputHandling(
     }
 
     // stderr
-    if ("ignore" !== outputChannelCodes.stderr) {
-        if (outputChannelCodes.stderr === outputChannelCodes.stdout) {
+    if ("ignore" !== outputHandlerConfigurations.stderr.handler) {
+        if (outputHandlerConfigurations.stderr === outputHandlerConfigurations.stdout) {
             // stderr should use the same channel instance as stdout.
             outputChannels.stderr = outputChannels.stdout;
         } else {
             // stderr uses a different channel than stdout.
             outputChannels.stderr = initializeOutputChannel(
-                outputChannelCodes.stderr,
+                outputHandlerConfigurations.stderr.handler,
                 plugin,
                 tShellCommand,
                 shellCommandParsingResult,
@@ -244,7 +244,7 @@ export function getOutputChannelClasses() {
 }
 
 export function initializeOutputChannel(
-        channelCode: OutputChannelCode,
+        channelCode: OutputHandlerCode,
         plugin: SC_Plugin,
         tShellCommand: TShellCommand,
         shellCommandParsingResult: ShellCommandParsingResult,
@@ -261,7 +261,7 @@ export function initializeOutputChannel(
     );
 }
 
-function registerOutputChannel(channelCode: OutputChannelCode, channelClass: typeof OutputChannel) {
+function registerOutputChannel(channelCode: OutputHandlerCode, channelClass: typeof OutputChannel) {
     if (undefined !== outputChannelClasses[channelCode]) {
         throw new Error("OutputChannel named '" + channelCode + "' is already registered!");
     }

src/Common.ts
@@ -104,7 +104,7 @@ export function getEditor(app: App): Editor | null {
 }
 
 // eslint-disable-next-line @typescript-eslint/ban-types
-export function cloneObject<ObjectType>(object: Object): ObjectType{
+export function cloneObject<ObjectType extends Object>(object: ObjectType): ObjectType{
     return Object.assign({}, object) as ObjectType;
 }
 

src/Migrations.ts
@@ -29,6 +29,8 @@ import {
     CustomVariableModel,
 } from "./models/custom_variable/CustomVariableModel";
 import {getModel} from "./models/Model";
+import {OutputStream} from "./output_channels/OutputHandlerCode";
+import {OutputChannel} from "./output_channels/OutputChannel";
 
 export async function RunMigrations(plugin: SC_Plugin) {
     const should_save = [ // If at least one of the values is true, saving will be triggered.
@@ -36,6 +38,7 @@ export async function RunMigrations(plugin: SC_Plugin) {
         MigrateCommandsToShellCommands(plugin),
         MigrateShellCommandsObjectToArray(plugin),
         MigrateShellCommandToPlatforms(plugin),
+        MigrateShellCommandOutputChannels(plugin),
         EnsureShellCommandsHaveAllFields(plugin),
         EnsureCustomVariablesHaveAllFields(plugin),
         DeleteEmptyCommandsField(plugin),
@@ -120,6 +123,30 @@ function MigrateCommandsToShellCommands(plugin: SC_Plugin) {
         }
     } else {
         debugLog("settings.commands is empty, so no need to migrate commands. Good thing! :)");
+    }
+    return save;
+}
+
+function MigrateShellCommandOutputChannels(plugin: SC_Plugin): boolean {
+    let save = false;
+    const shellCommandConfigurations = plugin.settings.shell_commands;
+    for (const shellCommandConfiguration of shellCommandConfigurations) {
+        let outputStream: OutputStream;
+        // Iterate "stdout" and "stderr".
+        // @ts-ignore
+        if (shellCommandConfiguration.output_channels) {
+            for (outputStream in shellCommandConfiguration.output_channels) {
+                const outputChannel = shellCommandConfiguration.output_channels[outputStream];
+                debugLog("Shell command #" + shellCommandConfiguration.id + ": Migrating output stream " + outputStream + " to use a configuration object.");
+                if (!shellCommandConfiguration.output_handlers) {
+                    // @ts-ignore Don't yell about the empty object, it will soon have content.
+                    shellCommandConfiguration.output_handlers = {};
+                }
+                shellCommandConfiguration.output_handlers[outputStream] = OutputChannel.getDefaultConfiguration(outputChannel);
+            }
+            delete shellCommandConfiguration.output_channels;
+            save = true;
+        }
     }
     return save;
 }

src/ShellCommandExecutor.ts
@@ -50,10 +50,10 @@ import {
     PlatformNames,
 } from "./settings/SC_MainSettings";
 import {
-    OutputChannelCode,
-    OutputChannelCodes,
+    OutputHandlerCode,
+    OutputHandlerConfigurations,
     OutputStream,
-} from "./output_channels/OutputChannelCode";
+} from "./output_channels/OutputHandlerCode";
 import {Readable} from "stream";
 import {Notice} from "obsidian";
 import {OutputChannel} from "./output_channels/OutputChannel";
@@ -72,7 +72,7 @@ export class ShellCommandExecutor {
     /**
      * Performs preactions, and if they all give resolved Promises, executes the shell command.
      */
-    public async doPreactionsAndExecuteShellCommand(parsing_process?: ShellCommandParsingProcess, overriding_output_channel?: OutputChannelCode) {
+    public async doPreactionsAndExecuteShellCommand(parsing_process?: ShellCommandParsingProcess, overriding_output_channel?: OutputHandlerCode) {
         const preactions = this.t_shell_command.getPreactions();
 
         // Does an already started ParsingProcess exist?
@@ -190,17 +190,19 @@ export class ShellCommandExecutor {
      * @param shell_command_parsing_result The actual shell command that will be executed is taken from this object's '.shell_command' property.
      * @param overriding_output_channel Optional. If specified, all output streams will be directed to this output channel. Otherwise, output channels are determined from this.t_shell_command.
      */
-    private executeShellCommand(shell_command_parsing_result: ShellCommandParsingResult, overriding_output_channel?: OutputChannelCode) {
+    private executeShellCommand(shell_command_parsing_result: ShellCommandParsingResult, overriding_output_channel?: OutputHandlerCode) {
         const working_directory = this.getWorkingDirectory();
 
         // Define output channels
-        let outputChannels = this.t_shell_command.getOutputChannels();
+        let outputHandlers = this.t_shell_command.getOutputHandlers();
         if (overriding_output_channel) {
-            // Ignore the shell command's normal channels and use temporarily something else.
-           outputChannels = {
-               'stdout': overriding_output_channel,
-               'stderr': overriding_output_channel,
+            // Ignore the shell command's normal output handlers and use temporarily something else.
+            outputHandlers = {
+               stdout: cloneObject(outputHandlers.stdout), // Clone output configurations to prevent ...
+               stderr: cloneObject(outputHandlers.stderr), // ... accidentally editing the originals.
            };
+            outputHandlers.stdout.handler = overriding_output_channel;
+            outputHandlers.stderr.handler = overriding_output_channel;
         }
 
         // Check that the shell command is not empty
@@ -303,13 +305,13 @@ export class ShellCommandExecutor {
                 switch (this.t_shell_command.getOutputHandlingMode()) {
                     case "buffered": {
                         // Output will be buffered and handled as a single batch.
-                        this.handleBufferedOutput(child_process, shell_command_parsing_result, outputChannels);
+                        this.handleBufferedOutput(child_process, shell_command_parsing_result, outputHandlers);
                         break;
                     }
 
                     case "realtime": {
                         // Output will be handled on-the-go.
-                        this.handleRealtimeOutput(child_process, shell_command_parsing_result, outputChannels, processTerminator);
+                        this.handleRealtimeOutput(child_process, shell_command_parsing_result, outputHandlers, processTerminator);
                     }
                 }
 
@@ -332,7 +334,7 @@ export class ShellCommandExecutor {
         }
     }
 
-    private handleBufferedOutput(child_process: ChildProcess, shell_command_parsing_result: ShellCommandParsingResult, outputChannels: OutputChannelCodes) {
+    private handleBufferedOutput(child_process: ChildProcess, shell_command_parsing_result: ShellCommandParsingResult, outputChannels: OutputHandlerConfigurations) {
         child_process.on("exit", (exitCode: number | null) => {
             // exitCode is null if user terminated the process. Reference: https://nodejs.org/api/child_process.html#event-exit (read on 2022-11-27).
 
@@ -390,7 +392,7 @@ export class ShellCommandExecutor {
     private handleRealtimeOutput(
             childProcess: ChildProcess,
             shell_command_parsing_result: ShellCommandParsingResult,
-            outputChannelCodes: OutputChannelCodes,
+            outputHandlerConfigurations: OutputHandlerConfigurations,
             processTerminator: (() => void) | null,
         ) {
 
@@ -399,7 +401,7 @@ export class ShellCommandExecutor {
             this.plugin,
             this.t_shell_command,
             shell_command_parsing_result,
-            outputChannelCodes,
+            outputHandlerConfigurations,
             processTerminator,
         );
 
@@ -441,13 +443,13 @@ export class ShellCommandExecutor {
         // Hook into exit events
         childProcess.on("exit", (exitCode: number, signal: string /* TODO: Pass signal to channels so it can be shown to users in the future */) => {
             // Call all OutputChannels' endRealtime().
-            const alreadyCalledChannelCodes: OutputChannelCode[] = [];
+            const alreadyCalledChannelCodes: OutputHandlerCode[] = [];
             for (const outputStreamName of Object.getOwnPropertyNames(outputChannels) as OutputStream[]) {
                 const outputChannel: OutputChannel | undefined = outputChannels[outputStreamName];
                 if (undefined === outputChannel) {
                     throw new Error("Output channel is undefined.");
                 }
-                const outputChannelCode: OutputChannelCode = outputChannelCodes[outputStreamName];
+                const outputChannelCode: OutputHandlerCode = outputHandlerConfigurations[outputStreamName].handler;
 
                 // Ensure this OutputChannel has not yet been called.
                 if (!alreadyCalledChannelCodes.includes(outputChannelCode)) {

src/TShellCommand.ts
@@ -49,7 +49,7 @@ import {
     PlatformNames,
 } from "./settings/SC_MainSettings";
 import {getIconHTML} from "./Icons";
-import {OutputStream} from "./output_channels/OutputChannelCode";
+import {OutputStream} from "./output_channels/OutputHandlerCode";
 import {OutputWrapper} from "./models/output_wrapper/OutputWrapper";
 
 export interface TShellCommandContainer {
@@ -194,8 +194,8 @@ export class TShellCommand {
         return this.configuration.output_channel_order;
     }
 
-    public getOutputChannels() {
-        return this.configuration.output_channels;
+    public getOutputHandlers() {
+        return this.configuration.output_handlers;
     }
 
     public getOutputHandlingMode() {

src/models/output_wrapper/OutputWrapperModel.ts
@@ -28,7 +28,7 @@ import {
 } from "./OutputWrapper";
 import {debugLog} from "../../Debug";
 import {getIDGenerator} from "../../IDGenerator";
-import {OutputStream} from "../../output_channels/OutputChannelCode";
+import {OutputStream} from "../../output_channels/OutputHandlerCode";
 import {OutputWrapperSettingsModal} from "./OutputWrapperSettingsModal";
 import {SC_MainSettings} from "../../settings/SC_MainSettings";
 

src/output_channels/OutputChannel.ts
@@ -21,9 +21,12 @@ import SC_Plugin from "../main";
 import {App} from "obsidian";
 import {OutputStreams} from "./OutputChannelFunctions";
 import {
+    OutputHandlerCode,
+    OutputHandlerConfiguration,
+    OutputHandlerConfigurations,
     OutputHandlingMode,
     OutputStream,
-} from "./OutputChannelCode";
+} from "./OutputHandlerCode";
 import {debugLog} from "../Debug";
 import {ShellCommandParsingResult, TShellCommand} from "../TShellCommand";
 import {joinObjectProperties} from "../Common";
@@ -295,6 +298,12 @@ export abstract class OutputChannel {
 
     public static() {
         return this.constructor as typeof OutputChannel;
+    }
+
+    public static getDefaultConfiguration(outputChannelCode: OutputHandlerCode): OutputHandlerConfiguration {
+        return {
+            handler: outputChannelCode,
+        };
     }
 }
 

src/output_channels/OutputChannel_Modal.ts
@@ -24,7 +24,7 @@ import {
     OutputStreams,
 } from "./OutputChannelFunctions";
 import {Setting, TextAreaComponent} from "obsidian";
-import {OutputChannelCode, OutputStream} from "./OutputChannelCode";
+import {OutputHandlerCode, OutputStream} from "./OutputHandlerCode";
 import SC_Plugin from "../main";
 import {ShellCommandParsingResult, TShellCommand} from "../TShellCommand";
 import {SC_Modal} from "../SC_Modal";
@@ -238,7 +238,7 @@ class OutputModal extends SC_Modal {
             .setClass("SC-output-channel-modal-redirection-buttons-container") // I think this calls actually HTMLDivElement.addClass(), so it should not override the previous .setClass().
         ;
         const outputChannels = getOutputChannelClasses();
-        Object.getOwnPropertyNames(outputChannels).forEach((output_channel_name: OutputChannelCode) => {
+        Object.getOwnPropertyNames(outputChannels).forEach((output_channel_name: OutputHandlerCode) => {
             const outputChannelClass = outputChannels[output_channel_name];
 
             // Ensure this channel is not excluded by checking that is has a hotkey defined.

commit 7f970a4d1ffe651fa484335854ec0710e7eec9b4

#330: OutputChannelFunctions.ts: Fix `stdout` and stderr `outputs` accidentally split to two OutputModals.
Commit b15a13837a095930593b1587b704a1d44bd4a862 caused the bug. Anyway, the bug did not end up to production.

src/output_channels/OutputChannelFunctions.ts
@@ -116,7 +116,7 @@ export function handleBufferedOutput(
     }
 
     // Should stderr be processed same time with stdout?
-    if (outputHandlers.stdout === outputHandlers.stderr) {
+    if (outputHandlers.stdout.handler === outputHandlers.stderr.handler) {
         // Stdout and stderr use the same channel.
         // Make one handling call.
         handle_stream(
@@ -206,7 +206,7 @@ export function startRealtimeOutputHandling(
 
     // stderr
     if ("ignore" !== outputHandlerConfigurations.stderr.handler) {
-        if (outputHandlerConfigurations.stderr === outputHandlerConfigurations.stdout) {
+        if (outputHandlerConfigurations.stderr.handler === outputHandlerConfigurations.stdout.handler) {
             // stderr should use the same channel instance as stdout.
             outputChannels.stderr = outputChannels.stdout;
         } else {